aea1e5c566973520a4418c34ae3208f2fd3d3c0c
Matteo Rocco <39690294+Mast86@users.noreply.github.com>
2023-04-28 11:52:19 +0200

    interface.py now initialize model in __init__ method.

--- pySM/interface/interface.py
+++ pySM/interface/interface.py
@@ -6,20 +6,20 @@ from pySM.src.model import Model
 
 class Interface:
 
-    file_settings_dir_path = Path(__file__).resolve().parent
+    default_file_settings_dir_path = Path(__file__).resolve().parent
+    default_file_settings_name = 'model_settings.json'
 
     def __init__(
             self,
-            log_level: str,
-            log_format: str,
-            file_settings_name: str = 'model_settings.json',
-            file_settings_dir_path: str = file_settings_dir_path,
-            log_file_name='log_model.log') -> None:
-        """Interface initialization: generating logger and file manager, 
-        loading model settings."""
+            log_level: str = 'info',
+            log_format: str = 'standard',
+            log_file_name: str = 'log_model.log',
+            file_settings_name: str = default_file_settings_name,
+            file_settings_dir_path: str = default_file_settings_dir_path,
+    ) -> None:
 
         self.logger = Logger(
-            logger_name='interface',
+            logger_name=str(self),
             log_level=log_level.upper(),
             log_file_path=Path(file_settings_dir_path) / log_file_name,
             log_format=log_format
@@ -28,10 +28,15 @@ class Interface:
         self.logger.info(f"Initializing '{str(self)}' object.")
 
         self.files = FileManager(logger=self.logger)
-        self.model = None
-        self.model_settings = self.files.load_file(
-            file_name=file_settings_name,
-            folder_path=file_settings_dir_path
+
+        self.file_settings_name = file_settings_name
+        self.file_settings_dir_path = file_settings_dir_path
+
+        self.model = Model(
+            logger=self.logger,
+            files=self.files,
+            file_settings_name=self.file_settings_name,
+            file_settings_dir_path=self.file_settings_dir_path,
         )
 
         self.logger.info(f"'{str(self)}' object correctly initialized.")
@@ -40,25 +45,13 @@ class Interface:
         class_name = type(self).__name__
         return f'{class_name}'
 
-    def model_init(self, generate_sets_file: bool) -> None:
-        """Initialization of the model and generation of blank sets file."""
-        if self.model is None:
-            self.model = Model(
-                logger=self.logger,
-                files=self.files,
-                model_settings=self.model_settings,
-                generate_sets_file=generate_sets_file)
-
     def model_cleanup(self):
-        """Deleting model data folder."""
         self.model.model_cleanup()
 
-    def generate_rps(self):
-        """Loading sets file and generating blank rps."""
-        self.model.database.load_sets()
-        # add validation of Category 
-        # eventually adding method for automatic ID completion in sets.
-        self.model.database.generate_blank_rps()
+    # def generate_rps(self):
+    #     self.model.database.load_sets()
+        # add validation of Category
+        # self.model.database.generate_blank_rps()
 
 
 if __name__ == '__main__':
