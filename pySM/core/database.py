b2c99bd6f713913ba6c4c7d1bb3ccec01a746cff
Matteo Rocco <39690294+Mast86@users.noreply.github.com>
2023-05-17 19:33:19 +0200

    added get_existing_tables method and added check for existing tables in __init__ before creating sets tables.

--- pySM/core/database.py
+++ pySM/core/database.py
@@ -2,8 +2,8 @@ import sqlite3
 import pandas as pd
 
 from typing import List, Dict
-from pySM.util import constants
 from pathlib import Path
+from pySM.util import constants
 from pySM.log_exc.logger import Logger
 from pySM.util.file_manager import FileManager
 
@@ -24,14 +24,17 @@ class Database:
 
         self.files = files
 
+        self.database_name = database_name
         self.database_dir_path = database_dir_path
-        self.database_path = Path(database_dir_path, database_name)
-
+        self.database_path = Path(database_dir_path, self.database_name)
         self.database_settings = database_settings
-        self.connection = sqlite3.connect(f'{self.database_path}')
-        self.cursor = self.connection.cursor()
+
+        self.connection = None
+        self.cursor = None
+        self.open_connection()
 
         self.sets_structure = constants._SETS
+        self.sets = None
 
         for table in self.sets_structure:
             self.create_table(
@@ -52,11 +55,24 @@ class Database:
         class_name = type(self).__name__
         return f'{class_name}'
 
+    def open_connection(self) -> None:
+        self.connection = sqlite3.connect(f'{self.database_path}')
+        self.cursor = self.connection.cursor()
+        self.logger.info(f"'{str(self)}' connection opened.")
+
+    def close_connection(self) -> None:
+        self.connection.close()
+        self.logger.info(f"'{str(self)}' connection closed.")
+
     def create_table(
             self,
             table_name: str,
             table_fields: Dict[str, str],
     ) -> None:
+
+        if table_name in self.get_existing_tables():
+            self.logger.debug(f"Table '{table_name}' already exists.")
+            return
 
         fields_str = ", ".join(
             [f'{field_name} {field_type}'
@@ -73,6 +89,12 @@ class Database:
             self.logger.error(error_msg)
             raise sqlite3.OperationalError(error_msg)
 
+    def get_existing_tables(self) -> List[str]:
+        query = "SELECT name FROM sqlite_master WHERE type='table'"
+        self.cursor.execute(query)
+        tables = self.cursor.fetchall()
+        return [table[0] for table in tables]
+
     def get_table_fields(
             self,
             table_name: str
@@ -80,15 +102,16 @@ class Database:
 
         table_fields = {}
         query = f"PRAGMA table_info('{table_name}')"
-
         self.cursor.execute(query)
         result = self.cursor.fetchall()
         table_fields['labels'] = [row[1] for row in result]
         table_fields['types'] = [row[2] for row in result]
-
         return table_fields
 
-    def count_table_data_entries(self, table_name: str) -> int:
+    def count_table_data_entries(
+            self,
+            table_name: str
+    ) -> int:
         self.cursor.execute(f'SELECT COUNT(*) FROM {table_name}')
         return self.cursor.fetchone()[0]
 
@@ -153,12 +176,8 @@ class Database:
 
         return df
 
-    def close_connection(self) -> None:
-        self.connection.close()
-        self.logger.debug(f"'{str(self)}' connection closed.")
-
-    def load_new_sets(self) -> Dict[str, pd.DataFrame]:
-        if self.database_settings['sets_definition_excel'] == True:
+    def load_sets(self) -> Dict[str, pd.DataFrame]:
+        if self.database_settings['sets_definition_excel']:
             self.sets = self.files.excel_to_dataframes_dict(
                 excel_file_name=self.database_settings['sets_file_name'],
                 excel_file_dir_path=self.database_dir_path,
@@ -169,6 +188,9 @@ class Database:
                     table_name=table,
                     df=self.sets[table],
                 )
+            self.logger.info(
+                "New sets loaded from "
+                f"'{self.database_settings['sets_file_name']}'.")
         else:
             self.sets = {table: self.table_to_dataframe(table_name=table)
                          for table in self.sets_structure}
@@ -178,12 +200,5 @@ class Database:
                 excel_dir_path=self.database_dir_path,
                 excel_file_name=self.database_settings['sets_file_name'],
             )
-
-    def update_sets(
-            self,
-            excel_to_database: bool = True,
-    ) -> None:
-        # if true, takes the excel sets file and update the database sql
-        # (delete entries not in the excel, add entries in the excel not in)
-        # the other way around is also possible with False
-        pass
+            self.logger.info(
+                f"New sets loaded from sqlite '{self.database_name}'.")
