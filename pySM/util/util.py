5daa2c1da2719f6de8118a79f433f5a3f150edce
Matteo Rocco <39690294+Mast86@users.noreply.github.com>
2023-06-12 17:50:07 +0200

    method write_excel deleted from utils (moved into database.py)

--- pySM/util/util.py
+++ pySM/util/util.py
@@ -1,6 +1,7 @@
 import pprint as pp
 import pandas as pd
 
+
 def prettify(item: dict) -> None:
     """Print a dictionary in a human-readable format in the terminal
 
@@ -27,27 +28,3 @@ def find_dict_depth(item) -> int:
     if not isinstance(item, dict) or not item:
         return 0
     return 1 + max(find_dict_depth(v) for k, v in item.items())
-
-
-def write_excel(
-          excel_file_path: str,
-          dict_name: dict,
-          headers: str,
-          writer_engine: str = 'openpyxl',
-
-          ):
-            """Support function to generate excel"""
-            with pd.ExcelWriter(excel_file_path, engine=writer_engine) as writer:
-                for sheet_name, value in dict_name.items():
-                    dataframe = pd.DataFrame(
-                        columns=value[headers])
-                    sheet = writer.book.create_sheet(sheet_name)
-                    writer.sheets[sheet_name] = sheet
-                    dataframe.to_excel(
-                        writer,
-                        sheet_name=sheet_name,
-                        index=False
-                    )
-
-if __name__ == '__main__':
-    pass
