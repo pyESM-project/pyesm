38babc38ee1f8a0317a0bc7a089659627f74d11f
Matteo Rocco <39690294+Mast86@users.noreply.github.com>
2024-01-17 10:59:23 +0100

    exceptions module: start collecting all exceptions in this module.

--- src/log_exc/exceptions.py
+++ src/log_exc/exceptions.py
@@ -1,8 +1,23 @@
-from sqlite3 import OperationalError
-
-
 class MissingDataError(Exception):
 
     def __init__(self, message='Missing data error.'):
         self.message = message
         super().__init__(self.message)
+
+
+class OperationalError(Exception):
+
+    def __init__(self, message='Operational error.'):
+        self.message = message
+        super().__init__(self.message)
+
+
+class IntegrityError(Exception):
+
+    def __init__(self, message='Integrity error'):
+        self.message = message
+        super().__init__(self.message)
+
+
+class TableNotFoundError(Exception):
+    pass
