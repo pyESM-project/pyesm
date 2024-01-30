7ab3f14f2833bd10b2705e99a3f51b5a7ccff617
Matteo Rocco <39690294+Mast86@users.noreply.github.com>
2024-01-30 10:00:45 +0100

    setup.py file added. ready for installation as a virtual package.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyESM',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'cvxpy',
+    ],
+    author='Matteo V. Rocco',
+    author_email='matteovincenzo.rocco@polimi.it',
+    description='...',
+    url='https://github.com/MIMO-modelling-suite/pyESM',
+)
