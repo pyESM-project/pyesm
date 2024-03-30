9abf2688dc73f1d642dda8e5539a876c72bcba05
Matteo Rocco <39690294+Mast86@users.noreply.github.com>
2024-03-30 11:22:49 +0100

    Adding possibility to avoid asking for user confirmation in main Model methods (for testing purpose, mainly). Structuring and docstringing test_workflow.py module. Now it is possible to add model cases in fixture folder to test if the model is capabable of being generated and run without errors.

--- tests/integrations/test_workflow.py
+++ tests/integrations/test_workflow.py
@@ -12,9 +12,21 @@ cases = [
 
 @pytest.fixture(params=cases, scope='module')
 def model_env(request):
+    """
+    A fixture that provides environment setup for Model instances based on 
+    different test cases. It yields a dictionary containing the model directory
+    name, the main directory path, and the log level for each test case defined
+    in the 'cases' list.
+
+    Parameters:
+        request: The request object for accessing the current test context.
+
+    Yields:
+        A dictionary essential Model settings.
+    """
     model_dir_name = request.param
     fixtures_dir = 'D:/git_repos/pyesm/tests/fixtures'
-    log_level = 'info'
+    log_level = 'debug'
 
     return {
         'model_dir_name': model_dir_name,
@@ -25,6 +37,18 @@ def model_env(request):
 
 @pytest.fixture(scope='module')
 def model_instance_existing_data(model_env):
+    """
+    A module-scoped fixture that initializes and returns a Model instance 
+    using existing data. It utilizes the 'model_env' fixture to retrieve the 
+    necessary environment setup.
+
+    Parameters:
+        model_env: The environment setup returned by the 'model_env' fixture.
+
+    Returns:
+        An instance of the Model class initialized with the provided 
+            environment setup.
+    """
     try:
         model = Model(
             model_dir_name=model_env['model_dir_name'],
@@ -38,32 +62,70 @@ def model_instance_existing_data(model_env):
     return model
 
 
-@patch('builtins.input', return_value='y')
-def test_update_database_and_problem(model_instance_existing_data):
+def test_update_database_and_problem(model_instance_existing_data: Model):
+    """
+    Tests the 'update_database_and_problem' method of the Model instance.
+    Verifies that the method can be called without throwing an exception.
+
+    Parameters:
+        model_instance_existing_data: A Model instance initialized with 
+            existing data.
+    """
     try:
-        model_instance_existing_data.update_database_and_problem()
+        model_instance_existing_data.update_database_and_problem(
+            force_overwrite=True
+        )
     except Exception as e:
         pytest.fail(
             f'An error occurring during database and problem update: {e}')
 
 
-@patch('builtins.input', return_value='y')
-def test_problem_initialization(model_instance_existing_data):
+def test_problem_initialization(model_instance_existing_data: Model):
+    """
+    Tests the problem initialization process of the Model instance.
+    Ensures that 'initialize_problems' can be executed without errors.
+
+    Parameters:
+        model_instance_existing_data: A Model instance initialized with 
+            existing data for testing.
+    """
     try:
-        model_instance_existing_data.initialize_problems()
+        model_instance_existing_data.initialize_problems(
+            force_overwrite=True
+        )
     except Exception as e:
         pytest.fail(
             f'An error occurring during problem initialization: {e}')
 
 
-def test_model_run(model_instance_existing_data):
+def test_model_run(model_instance_existing_data: Model):
+    """
+    Tests the model run functionality.
+    Asserts that calling 'run_model' on the Model instance does not 
+    result in errors.
+    NOTICE THAT the test only checks for the correct execution of the method,
+    but not if the data resulting from model run are as expected.
+
+    Parameters:
+        model_instance_existing_data: A Model instance to be tested for the 
+            model run process.
+    """
     try:
         model_instance_existing_data.run_model(verbose=False)
     except Exception as e:
         pytest.fail(f'An error occurring during model run: {e}')
 
 
-def test_results_export(model_instance_existing_data):
+def test_results_export(model_instance_existing_data: Model):
+    """
+    Tests the export of results to the database by the Model instance.
+    Confirms that 'load_results_to_database' can be executed without 
+    encountering errors.
+
+    Parameters:
+        model_instance_existing_data: A Model instance from which results 
+            are to be exported.
+    """
     try:
         model_instance_existing_data.load_results_to_database()
     except Exception as e:
