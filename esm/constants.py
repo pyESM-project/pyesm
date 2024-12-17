8c63f7ba5ecea146abdc216ffd68e1dd3056eb6b
Matteo Rocco <39690294+Mast86@users.noreply.github.com>
2024-12-17 11:42:20 +0100

    Constants class improved by defining better structures for sets, datatables, problems. Such structures serve for validation and for generating templates for data input. Data structures have properly commented (default files are now no longer necessary.

--- esm/constants.py
+++ esm/constants.py
@@ -39,7 +39,7 @@ class Constants:
         }
         SETUP_XLSX_FILE = 'model_settings.xlsx'
         SETS_FILE = 'sets.xlsx'
-        AVAILABLE_SOURCES = ['yaml', 'xlsx']
+        AVAILABLE_SOURCES = ['yml', 'xlsx']
         INPUT_DATA_DIR = 'input_data'
         INPUT_DATA_FILE = 'input_data.xlsx'
         DATA_FILES_EXTENSION = '.xlsx'
@@ -48,65 +48,100 @@ class Constants:
         TUTORIAL_FILE_NAME = 'tutorial.ipynb'
         DEFAULT_MODELS_DIR_PATH = 'default'
 
-    class Headers:
+    class Labels:
         """Standard headers and field names."""
-        NAME_HEADER = 'name'
-        FILTERS_HEADERS = 'filters'
-        AGGREGATION_HEADER = 'aggregation'
+        NAME = 'name'
+        FILTERS = 'filters'
+        AGGREGATION = 'aggregation'
+        CVXPY_VAR = 'variable'
+        SUB_PROBLEM_KEY = 'sub_problem_key'
+        FILTER_DICT_KEY = 'filter'
+        PROBLEM = 'problem'
+        PROBLEM_INFO = 'info'
+        PROBLEM_STATUS = 'status'
+        OBJECTIVE = 'objective'
+        CONSTRAINTS = 'expressions'
+
         GENERIC_FIELD_TYPE = 'TEXT'
         VALUES_FIELD = {'values': ['values', 'REAL']}
         ID_FIELD = {'id': ['id', 'INTEGER PRIMARY KEY']}
-        CVXPY_VAR_HEADER = 'variable'
-        FILTER_DICT_HEADER = 'filter'
-        SUB_PROBLEM_KEY_HEADER = 'sub_problem_key'
-        PROBLEM_HEADER = 'problem'
-        PROBLEM_INFO_HEADER = 'info'
-        PROBLEM_STATUS_HEADER = 'status'
-        OBJECTIVE_HEADER = 'objective'
-        CONSTRAINTS_HEADER = 'expressions'
+
+        SET_TABLE_NAME_PREFIX = '_set_'
+        COLUMN_NAME_SUFFIX = '_Name'
+        COLUMN_AGGREGATION_SUFFIX = '_Aggregation'
 
     class DefaultStructures:
         """Default structures for data validation."""
         OPTIONAL = object()
         ANY = object()
 
-        SET_STRUCTURE = {
-            'symbol': str,
-            'table_name': str,
-            'split_problem': (OPTIONAL, bool),
-            'copy_from': (OPTIONAL, str),
-            'table_structure': {
-                'name': str,
-                'aggregation': (OPTIONAL, str),
-                'filters': (OPTIONAL, {
-                    ANY: {
-                        'headers': str,
-                        'values': list,
-                    }
-                })
+        SET_STRUCTURE = (
+            # set name (case insensitive: 'e' and 'E' are the same set)
+            'set_key:',
+            {
+                # metadata
+                'description': (OPTIONAL, str),
+                # in case the set defines independent numerical sub-problems
+                'split_problem': (OPTIONAL, bool),
+                # key of another set to copy the data from
+                'copy_from': (OPTIONAL, str),
+                # dictionary with keys as the filters name and values as the
+                # list of filter values
+                'filters': (OPTIONAL, {ANY: list})
             }
-        }
-        DATA_TABLE_STRUCTURE = {
-            'name': str,
-            'type': (str, dict),
-            'integer': (OPTIONAL, bool),
-            'coordinates': list,
-            'variables_info': {
-                ANY: {
-                    'value': (OPTIONAL, str),
-                    ANY: (OPTIONAL, {
-                        'dim': (OPTIONAL, str),
-                        'filters': (OPTIONAL, dict),
-                    })
+        )
+
+        DATA_TABLE_STRUCTURE = (
+            # data table name (case insensitive: 'e' and 'E' are the same table)
+            'table_key:',
+            {
+                # metadata
+                'description': str,
+                # ALLOWED_VARIABLES_TYPES (or dictionary with keys as problem
+                # name and corresponding values as allowed types)
+                'type': (str, dict),
+                # if variables of the table are integers (default: False)
+                'integer': (OPTIONAL, bool),
+                # list of table coordinates (set_key symbols)
+                'coordinates': list,
+                # definition of the variables based on the same data table
+                'variables_info': {
+                    # dictionary with keys as variables names and values as dict
+                    # with variable info. Variables are case sensitive.
+                    ANY: {
+                        # ALLOWED_CONSTANTS (only for constants!)
+                        'value': (OPTIONAL, str),
+                        # dictionary with keys as set_key symbols and values
+                        # defining the dimension and filters for the set
+                        ANY: (OPTIONAL, {
+                            # ALLOWED_DIMENSIONS (included in 'coordinates')
+                            'dim': (OPTIONAL, str),
+                            # dictionary with keys as the filters key of the set
+                            # and values as the list of values to filter
+                            'filters': (OPTIONAL, dict),
+                        })
+                    }
                 }
             }
-        }
+        )
+
+        PROBLEM_STRUCTURE = (
+            # problem name defined in case of multiple problems
+            'problem_key: # optional',
+            {
+                # Minimize() or Maximize() with an expression resulting in a scalar
+                'objective': (OPTIONAL, list),
+                # definition of additional expressions (equalities/inequalities)
+                'expressions': list,
+            }
+        )
 
     class SymbolicDefinitions:
         """Allowed constants and operators for symbolic problem definitions."""
         ALLOWED_DIMENSIONS = ['rows', 'cols', 'intra', 'inter']
         ALLOWED_VARIABLES_TYPES = ['constant', 'exogenous', 'endogenous']
         REGEX_PATTERN = r'\b[a-zA-Z_][a-zA-Z0-9_]*\b'
+
         ALLOWED_CONSTANTS = {
             'sum_vector': (np.ones, {}),
             'identity': (np.eye, {}),
@@ -151,12 +186,17 @@ class Constants:
         MAXIMUM_ITERATIONS_MODEL_COUPLING = 20
         ROUNDING_DIGITS_RELATIVE_DIFFERENCE_DB = 5
 
+    class TextNotes:
+        """Text notes and messages for user."""
+        GENERIC_NOTE = ("")
+
     _SUBGROUPS = [
         ConfigFiles,
-        Headers,
+        Labels,
         DefaultStructures,
         SymbolicDefinitions,
-        NumericalSettings
+        NumericalSettings,
+        TextNotes,
     ]
 
     @classmethod
